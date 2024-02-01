03c3ec7e4d28072e87a4c493026af522f19bb2e0
Raúl <raul.marrupe1@educa.madrid.org>
2024-02-01 13:25:30 +0100

    yippeee

--- js/app.js
+++ js/app.js
@@ -1,26 +1,14 @@
-function createNElements(n) {
+
+var ejArray = [];
+var br = document.createElement("br");
+function crearEjercicios(n) {
   for (let i = 0; i < n; i++) {
-    document.body.appendChild(document.createElement("ej" +i))
-  }
+    ejArray[i] = document.createElement("a")
+    ejArray[i].setAttribute("href", ("./ejercicio" + (i + 1).toString() + ".html"));
+    ejArray[i].appendChild(br);
+    ejArray[i].innerHTML += "Ejercicio " + (i+1);
+    document.body.appendChild(ejArray[i])
+    }
 }
 
-createNElements(13)
-let regExpTagNameEj = /[0-9]/i
-var bodyChildren = document.body.childNodes
-var paragraphElements = bodyChildren.item(2).nodeName
-
-
-function fuckgin() {
-  document.body.innerHTML += paragraphElements
-  if (paragraphElements == ("EJ0")) {
-    document.body.innerHTML += "woaw"
-  }
-}
-fuckgin()
-function addInfoInBodyChildren() {
-  for (let i = 0; i < bodyChildren.length; i++) {
-    bodyChildren.item(i).innerHTML += "Ejercicio " + i;
-  }
-}
-
-//addInfoInBodyChildren();
+crearEjercicios(8)
