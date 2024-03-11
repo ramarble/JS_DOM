40ba4fe7a9caaead3f97c03f7a7c26ba919fbb9a
Raúl <raul.marrupe1@educa.madrid.org>
2024-03-11 14:11:08 +0100

    srg

--- trimestre3/js/ejercicio2.js
+++ trimestre3/js/ejercicio2.js
@@ -1,5 +1,11 @@
-let jsonFile = fetch("../json/ej3.json")
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error fetching JSON:', error));
-console.log(jsonFile)
+var jsonObj = '{"Nombre": "John", "Apellido": "Smith", "Edad": "22", "Dirección": {"Calle": "Gran vía 22","Ciudad": "Nueva York","Estado": {"CodigoPostal": 10021,"Nombre": "NY"}},"Telefono": [{"Tipo": "Casa", "Numero": "212 555-1234"},{"Tipo": "Fax","Numero": "646 555-4567"}]}';
+
+var actual = {"Nombre": "John", "Apellido": "Smith", "Edad": "22", "Dirección": {"Calle": "Gran vía 22","Ciudad": "Nueva York","Estado": {"CodigoPostal": 10021,"Nombre": "NY"}},"Telefono": [{"Tipo": "Casa", "Numero": "212 555-1234"},{"Tipo": "Fax","Numero": "646 555-4567"}]};
+
+let obj = JSON.parse(jsonObj);
+let showText = document.getElementById("cringe");
+
+showText.innerHTML += actual.Nombre + " " + actual.Apellido + " ";
+
+showText.innerHTML += obj.Nombre + " " + obj.Apellido;
+
