87c5dd51032b4a360e283cab3e253783db82610e
Raúl <raul.marrupe1@educa.madrid.org>
2024-03-18 11:11:49 +0100

    me duele la cabeza :D

---
+++ trimestre3/js/ejercicio4.js
@@ -0,0 +1,22 @@
+let arr = {};
+let formu = document.getElementById("formu").childNodes
+let obj = [];
+function stuffs() {
+    console.log(formu[1].placeholder)
+    let outsidecounter = 0
+    for (let index = 1; index < formu.length; index++) {
+
+
+    console.log(obj)
+    }
+
+}
+
+function save() {
+    stuffs()
+
+}
+
+function load() {
+
+}
