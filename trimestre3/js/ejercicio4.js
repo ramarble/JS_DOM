7e25837fcff76a1077484a32f3c680844d4b4779
Raúl <raul.marrupe1@educa.madrid.org>
2024-04-04 13:53:29 +0200

    g

--- trimestre3/js/ejercicio4.js
+++ trimestre3/js/ejercicio4.js
@@ -1,22 +1,42 @@
-let arr = {};
-let formu = document.getElementById("formu").childNodes
-let obj = [];
+
+let storedFormInfo = [];
+let storedFormInfoTheSecond = [];
+let form = document.getElementById("form")
+let formChildren = form.children
 function stuffs() {
-    console.log(formu[1].placeholder)
-    let outsidecounter = 0
-    for (let index = 1; index < formu.length; index++) {
-
-
-    console.log(obj)
+    storedFormInfo.push({});
+    for (let index = 0; index < formChildren.length; index++) {
+        eval("storedFormInfo[" + (storedFormInfo.length-1) + "]." + formChildren[index].id + 
+            " = formChildren.item("+ index + ").value") 
     }
+    console.log(storedFormInfo[storedFormInfo.length-1])
+}
 
+let defaultObj = {nombre: null, apellido: null, apellido2: null, email: null, telefono: null, nomPerfil: null}
+
+function stuffsthesecond() {
+    var buffer = defaultObj;
+    buffer.nombre = formChildren.item(0).value;
+    buffer.apellido = formChildren.item(1).value;
+    buffer.apellido2 = formChildren.item(2).value;
+    buffer.email = formChildren.item(3).value;
+    buffer.telefono = formChildren.item(4).value;
+    buffer.nomPerfil = formChildren.item(5).value;
+    storedFormInfoTheSecond.push(buffer);
+    console.log(storedFormInfoTheSecond)
 }
 
 function save() {
-    stuffs()
+    //stuffs()
+    stuffsthesecond()
 
 }
 
-function load() {
+function reset() {
+    form.reset();
+}
 
+function load() {
+//mumeidead
+    
 }
